35c566dad283f3d8cfa2016ab2274a67375dddb4
Tatsuhiko Arai <tatsu@tatsu.com>
2016-06-04 08:27:31 +0900

    Initial commit

---
+++ OpenSphericalCameraSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenSphericalCameraSample
+//
+//  Created by Tatsuhiko Arai on 6/3/16.
+//  Copyright © 2016 Tatsuhiko Arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
